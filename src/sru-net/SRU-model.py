4fafe58d8a8deb8de5e38f36d66f79eef0f84ea7
David <78443646+wittmajerd@users.noreply.github.com>
2024-05-04 15:51:44 +0200

    more cleanup, srunet16

--- src/sru-net/SRU-model.py
+++ src/sru-net/SRU-model.py
@@ -91,7 +91,7 @@ class UNet4(nn.Module):
         x = self.up5(x, x0)
         x = self.up6(x, x_1)
         x = self.outc(x)
-        # Is this sigmoid necessary?
+
         return torch.sigmoid(x)
 
     def weight_init(self, mean, std):
@@ -109,56 +109,96 @@ class UNet8(nn.Module):
         self.up1 = up(1024, 256)
         self.up2 = up(512, 128)
         self.up3 = up(256, 64)
-        self.up4 = up(128, 32)#(128, 64)
+        self.up4 = up(128, 32)  #(128, 64)
         self.up5 = up(64, 16)
         self.up6 = up(32,8)
         self.up7 = up(16,8)
-        self.outc = outconv(8, n_classes)#64
+        self.outc = outconv(8, n_classes)   #64
 
         self.up_s1=up_s(64,32)
         self.up_s2=up_s(32,16)
         self.up_s3=up_s(16,8)
 
     def forward(self, x):
-        #print(x.shape)
         x1 = self.inc(x)
-        #print(x1.shape)
         x2 = self.down1(x1)
-        #print(x2.shape)
         x3 = self.down2(x2)
-        #print(x3.shape)
         x4 = self.down3(x3)
-        #print(x4.shape)
         x5 = self.down4(x4)
-        #print("x5")
-        #print(x5.shape)
 
         x = self.up1(x5, x4)
-        #print(x.shape)
         x = self.up2(x, x3)
-        #print(x.shape)
         x = self.up3(x, x2)
-        #print(x.shape)
         x = self.up4(x, x1)
-        #print(x.shape)
-        #print("x0")
+
         x0=self.up_s1(x1)
         x_1=self.up_s2(x0)
         x_2=self.up_s3(x_1)
-        #print(x0.shape)
+
         x = self.up5(x, x0)
         x = self.up6(x, x_1)
         x = self.up7(x,x_2)
-        #print("up5")
-        #print(x.shape)
         x = self.outc(x)
-        #print("outc")
-        #print(x.shape)
+
         return torch.sigmoid(x)
 
     def weight_init(self, mean, std):
         for m in self._modules:
             normal_init(self._modules[m], mean, std)
+
+class UNet16(nn.Module):
+    def __init__(self, n_channels, n_classes):
+        super(UNet8, self).__init__()
+        self.inc = inconv(n_channels, 64)
+        self.down1 = down(64, 128)
+        self.down2 = down(128, 256)
+        self.down3 = down(256, 512)
+        self.down4 = down(512, 512)
+        self.up1 = up(1024, 256)
+        self.up2 = up(512, 128)
+        self.up3 = up(256, 64)
+        self.up4 = up(128, 32)  #(128, 64)
+        self.up5 = up(64, 16)
+        self.up6 = up(32,8)
+        self.up7 = up(16,8)
+        self.up8 = up(8,8)
+        self.outc = outconv(8, n_classes)   #64
+
+        self.up_s1=up_s(64,32)
+        self.up_s2=up_s(32,16)
+        self.up_s3=up_s(16,8)
+        self.up_s4=up_s(8,8)
+
+    def forward(self, x):
+        x1 = self.inc(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+
+        x = self.up1(x5, x4)
+        x = self.up2(x, x3)
+        x = self.up3(x, x2)
+        x = self.up4(x, x1)
+
+        x0=self.up_s1(x1)
+        x_1=self.up_s2(x0)
+        x_2=self.up_s3(x_1)
+        x_3=self.up_s3(x_2)
+
+        x = self.up5(x, x0)
+        x = self.up6(x, x_1)
+        x = self.up7(x, x_2)
+        x = self.up8(x, x_3)
+        x = self.outc(x)
+
+        return torch.sigmoid(x)
+
+    def weight_init(self, mean, std):
+        for m in self._modules:
+            normal_init(self._modules[m], mean, std)
+
+
 
 def normal_init(m, mean, std):
     if isinstance(m, nn.ConvTranspose2d) or isinstance(m, nn.Conv2d):
