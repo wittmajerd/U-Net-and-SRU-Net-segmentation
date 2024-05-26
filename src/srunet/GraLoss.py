cf3df4f4e01383bfb715478e51dcdc747cc9f004
David <78443646+wittmajerd@users.noreply.github.com>
2024-05-26 09:15:29 +0200

    graloss for 1 channel

--- src/srunet/GraLoss.py
+++ src/srunet/GraLoss.py
@@ -22,47 +22,26 @@ class GradientLoss(nn.Module):
         self.weight2 = nn.Parameter(data=kernel2, requires_grad=False)
 
     def forward(self, x, y):
+        loss1 = 0
+        loss2 = 0
 
-        x1 = x[:, 0]
-        x2 = x[:, 1]
-        x3 = x[:, 2]
-        x1 = F.conv2d(x1.unsqueeze(1), self.weight1, padding=2)
-        x2 = F.conv2d(x2.unsqueeze(1), self.weight1, padding=2)
-        x3 = F.conv2d(x3.unsqueeze(1), self.weight1, padding=2)
+        for i in range(x.shape[0]):  # loop over each image in the batch
+            x1 = x[i, 0].float()
+            x1 = F.conv2d(x1.unsqueeze(0).unsqueeze(0), self.weight1, padding=1)
 
-        x = torch.cat([x1, x2, x3], dim=1)
+            y1 = y[i, 0].float()
+            y1 = F.conv2d(y1.unsqueeze(0).unsqueeze(0), self.weight1, padding=1)
 
-        y1 = y[:, 0]
-        y2 = y[:, 1]
-        y3 = y[:, 2]
-        y1 = F.conv2d(y1.unsqueeze(1), self.weight1, padding=2)
-        y2 = F.conv2d(y2.unsqueeze(1), self.weight1, padding=2)
-        y3 = F.conv2d(y3.unsqueeze(1), self.weight1, padding=2)
+            loss1 += torch.mean(torch.mean((x1 - y1)**2)) / 100.0
 
-        y = torch.cat([y1, y2, y3], dim=1)
-        loss1=torch.mean(torch.mean((x-y)**2))/100.0
+            x1 = F.conv2d(x1, self.weight2, padding=1)
 
-        x1 = x[:, 0]
-        x2 = x[:, 1]
-        x3 = x[:, 2]
-        x1 = F.conv2d(x1.unsqueeze(1), self.weight2, padding=2)
-        x2 = F.conv2d(x2.unsqueeze(1), self.weight2, padding=2)
-        x3 = F.conv2d(x3.unsqueeze(1), self.weight2, padding=2)
+            y1 = F.conv2d(y1, self.weight2, padding=1)
 
-        x = torch.cat([x1, x2, x3], dim=1)
+            loss2 += torch.mean(torch.mean((x1 - y1)**2)) / 10000.0
 
-        y1 = y[:, 0]
-        y2 = y[:, 1]
-        y3 = y[:, 2]
-        y1 = F.conv2d(y1.unsqueeze(1), self.weight2, padding=2)
-        y2 = F.conv2d(y2.unsqueeze(1), self.weight2, padding=2)
-        y3 = F.conv2d(y3.unsqueeze(1), self.weight2, padding=2)
+        loss1 /= x.shape[0]  # average the loss over the batch
+        loss2 /= x.shape[0]  # average the loss over the batch
 
-        y = torch.cat([y1, y2, y3], dim=1)
-        loss2=torch.mean(torch.mean((x-y)**2))/10000.0
-
-        loss=torch.sqrt(loss1*loss1+loss2*loss2)*2
-        #print(loss)
-        #loss=(torch.log(loss))/100.0
-        #print(loss)
+        loss = torch.sqrt(loss1*loss1 + loss2*loss2) * 2
         return loss
