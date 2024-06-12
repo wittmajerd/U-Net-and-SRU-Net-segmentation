7d8eca6b628a14120e283f77ea3a44b93dc4dc3c
David <78443646+wittmajerd@users.noreply.github.com>
2024-06-12 12:56:01 +0200

    updated linear subsampling

--- src/datasets.py
+++ src/datasets.py
@@ -16,7 +16,6 @@ def create_datasets(path, train_percent, mask_type, test_percent=0, biosensor_le
         val_size = val_size - test_size
         train_files, val_files, test_files = torch.utils.data.random_split(files, [train_size, val_size, test_size])
 
-
     mean, std = calculate_mean_and_std(path, train_files, biosensor_length)
 
     train_dataset = BiosensorDataset(path, train_files, mean, std, mask_type, biosensor_length, mask_size, augment)
@@ -26,6 +25,13 @@ def create_datasets(path, train_percent, mask_type, test_percent=0, biosensor_le
         return train_dataset, val_dataset, test_dataset
     return train_dataset, val_dataset
 
+# Gives a linearly spaced indices array of length length for the biosensor subsampling
+# n is the length of the original biosensor
+# length is the length of the subsampled biosensor
+# The indices are 1-indexed because the first frame would be empty so we skip it
+def lin_indices(n, length):
+    indices = np.linspace(0, n - 1, length + 1, dtype=int)
+    return indices[1:]
 
 def calculate_mean_and_std(path, train_files, biosensor_length=16):
     # Preallocate a tensor of the correct size
@@ -35,7 +41,8 @@ def calculate_mean_and_std(path, train_files, biosensor_length=16):
         loaded_data = np.load(path + file)
         # Get the biosensor data with the correct length
         biosensor = torch.from_numpy(loaded_data['biosensor'].astype(np.float32))
-        indices = np.linspace(0, biosensor.shape[0] - 1, biosensor_length, dtype=int)
+        # indices = np.linspace(0, biosensor.shape[0] - 1, biosensor_length, dtype=int)
+        indices = lin_indices(biosensor.shape[0], biosensor_length)
         bio = biosensor[indices]
         # Fill the preallocated tensor
         data[i] = bio
@@ -74,7 +81,8 @@ class BiosensorDataset(Dataset):
         return len(self.files)
     
     def uniform_time_dim(self, biosensor):
-        indices = np.linspace(0, biosensor.shape[0] - 1, self.length, dtype=int)
+        # indices = np.linspace(0, biosensor.shape[0] - 1, self.length, dtype=int)
+        indices = lin_indices(biosensor.shape[0], self.length)
         return biosensor[indices]
     
     def uniform_mask(self, mask, centers):
