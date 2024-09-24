5dde5fa4f2497a21f63ee3e4b4a5bffddf932f4b
David <78443646+wittmajerd@users.noreply.github.com>
2024-09-24 13:54:09 +0200

    easily changeable models

--- src/test_models/test_parts.py
+++ src/test_models/test_parts.py
@@ -2,6 +2,20 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+class SingleConv(nn.Module):
+    """(convolution => [BN] => ReLU)"""
+
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.single_conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+        )
+
+    def forward(self, x):
+        return self.single_conv(x)
+
 class DoubleConv(nn.Module):
     """(convolution => [BN] => ReLU) * 2"""
 
@@ -20,20 +34,6 @@ class DoubleConv(nn.Module):
 
     def forward(self, x):
         return self.double_conv(x)
-
-class SingleConv(nn.Module):
-    """(convolution => [BN] => ReLU)"""
-
-    def __init__(self, in_channels, out_channels):
-        super().__init__()
-        self.single_conv = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1, bias=False),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU(inplace=True),
-        )
-
-    def forward(self, x):
-        return self.single_conv(x)
 
 class TripleConv(nn.Module):
     """(convolution => [BN] => ReLU) * 3"""
@@ -57,78 +57,33 @@ class TripleConv(nn.Module):
     def forward(self, x):
         return self.triple_conv(x)
 
-
 class Down(nn.Module):
-    """Downscaling with maxpool then double conv"""
+    """Downscaling with maxpool then a given conv"""
 
-    def __init__(self, in_channels, out_channels, mid_channels = None):
+    def __init__(self, in_channels, out_channels, Conv: nn.Module = DoubleConv):
         super().__init__()
         self.maxpool_conv = nn.Sequential(
             nn.MaxPool2d(2),
-            DoubleConv(in_channels, out_channels, mid_channels = mid_channels)
+            Conv(in_channels, out_channels)
         )
 
     def forward(self, x):
         return self.maxpool_conv(x)
-
-class DownSingle(nn.Module):
-    """Downscaling with maxpool then single conv"""
-
-    def __init__(self, in_channels, out_channels):
-        super().__init__()
-        self.maxpool_conv = nn.Sequential(
-            nn.MaxPool2d(2),
-            SingleConv(in_channels, out_channels)
-        )
-
-    def forward(self, x):
-        return self.maxpool_conv(x)
-
 
 class Up(nn.Module):
-    """Upscaling then double conv"""
+    """Upscaling then a given conv"""
 
-    def __init__(self, in_channels, out_channels, bilinear=True, mid_channels = None):
+    def __init__(self, in_channels, out_channels, Conv: nn.Module = DoubleConv, bilinear=True):
         super().__init__()
 
         # if bilinear, use the normal convolutions to reduce the number of channels
         # if not then there are weights in the upsampling so it learns the upsampling
         if bilinear:
             self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
-            self.conv = DoubleConv(in_channels, out_channels, in_channels // 2)
+            self.conv = Conv(in_channels, out_channels)
         else:
             self.up = nn.ConvTranspose2d(in_channels, in_channels // 2, kernel_size=2, stride=2)
-            self.conv = DoubleConv(in_channels, out_channels, mid_channels = mid_channels)
-
-    def forward(self, x1, x2):
-        x1 = self.up(x1)
-        # input is CHW
-        diffY = x2.size()[2] - x1.size()[2]
-        diffX = x2.size()[3] - x1.size()[3]
-
-        x1 = F.pad(x1, [diffX // 2, diffX - diffX // 2,
-                        diffY // 2, diffY - diffY // 2])
-        # if you have padding issues, see
-        # https://github.com/HaiyongJiang/U-Net-Pytorch-Unstructured-Buggy/commit/0e854509c2cea854e247a9c615f175f76fbb2e3a
-        # https://github.com/xiaopeng-liao/Pytorch-UNet/commit/8ebac70e633bac59fc22bb5195e513d5832fb3bd
-        
-        x = torch.cat([x2, x1], dim=1)
-        return self.conv(x)
-
-class UpSingle(nn.Module):
-    """Upscaling then single conv"""
-
-    def __init__(self, in_channels, out_channels, bilinear=True):
-        super().__init__()
-
-        # if bilinear, use the normal convolutions to reduce the number of channels
-        # if not then there are weights in the upsampling so it learns the upsampling
-        if bilinear:
-            self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
-            self.conv = SingleConv(in_channels, out_channels)
-        else:
-            self.up = nn.ConvTranspose2d(in_channels // 2, in_channels // 2, kernel_size=2, stride=2)
-            self.conv = SingleConv(in_channels, out_channels)
+            self.conv = Conv(in_channels, out_channels)
 
     def forward(self, x1, x2):
         x1 = self.up(x1)
@@ -149,20 +104,20 @@ class UpSingle(nn.Module):
 class Upscaling(nn.Module):
     """ Upscaling the image for the SRU-Net """
 
-    def __init__(self, in_channels, out_channels, bilinear=True):
+    def __init__(self, in_channels, out_channels, Conv: nn.Module = SingleConv, bilinear=True):
         super().__init__()
         # The bilinear interpolation is used to upscale the image simply
         if bilinear:
             self.upconv = nn.Sequential(
                 nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True),
-                SingleConv(in_channels, out_channels)
+                Conv(in_channels, out_channels)
             )
         # The transposed convolution is used to learn how to upscale the image best
         # Only if the upscaling is needed to be learnable
         else:
             self.upconv = nn.Sequential(
                 nn.ConvTranspose2d(in_channels // 2, out_channels // 2, kernel_size=2, stride=2),
-                SingleConv(in_channels, out_channels)
+                Conv(in_channels, out_channels)
             )
 
     def forward(self, x):
