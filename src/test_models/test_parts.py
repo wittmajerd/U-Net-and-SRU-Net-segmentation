7c12ebfaad21e126b68652f9ca1efb1b9f0955d0
David <78443646+wittmajerd@users.noreply.github.com>
2024-09-29 19:39:41 +0200

    quad conv

--- src/test_models/test_parts.py
+++ src/test_models/test_parts.py
@@ -57,6 +57,31 @@ class TripleConv(nn.Module):
     def forward(self, x):
         return self.triple_conv(x)
 
+class QuadConv(nn.Module):
+    """(convolution => [BN] => ReLU) * 4"""
+    def __init__(self, in_channels, out_channels, mid_channels=None):
+        super().__init__()
+        if not mid_channels:
+            mid_channels = out_channels
+        self.quad_conv = nn.Sequential(
+            nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(mid_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(mid_channels, mid_channels, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(mid_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        return self.quad_conv(x)
+
+
 class Down(nn.Module):
     """Downscaling with maxpool then a given conv"""
 
